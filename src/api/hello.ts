899c7af582213c6e478e848fca87d7081a3e2bda
BARK Protocol <177503488+barkprotocol@users.noreply.github.com>
2024-09-03 09:10:47 +0000

    Refactored

--- src/api/hello.ts
+++
@@ -1,7 +0,0 @@
-import type { NextApiRequest, NextApiResponse } from 'next';
-require('dotenv').config();
-
-export default function handler(req: NextApiRequest, res: NextApiResponse) {
-  const databaseUrl = process.env.DATABASE_URL;
-  res.status(200).json({ databaseUrl });
-}
