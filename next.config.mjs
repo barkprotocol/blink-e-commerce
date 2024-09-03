5dc2e997cc9aff80ad2d4863e43c9e1dae4f5e83
BARK Protocol <177503488+barkprotocol@users.noreply.github.com>
2024-09-03 09:42:10 +0000

    Added sample env

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,27 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-export default nextConfig;
+const nextConfig = {
+    async rewrites() {
+      return [
+        {
+          source: '/api/:path*',
+          destination: 'http://orange-orbit-7vrjx6wwwqq6hx9x-3000.app.github.dev/api/:path*',
+        },
+      ];
+    },
+    async headers() {
+      return [
+        {
+          source: '/(.*)',
+          headers: [
+            {
+              key: 'X-Custom-Header',
+              value: 'bark-custom-header-value',
+            },
+          ],
+        },
+      ];
+    },
+  };
+  
+  export default nextConfig;
+  
